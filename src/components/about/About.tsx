7e1dd38852db0b1b6223691a4429b0269bbbe093
Felix Gustafsson <59607803+FelixGustafsson@users.noreply.github.com>
2023-12-23 16:11:14 +0100

    Update About.tsx

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -19,18 +19,17 @@ const About = () => {
         <br />
         <br />
         I'm Currently living in Lund, Sweden. I've always had a passion for
-        technology and especially programming since I was 10. That's when my
+        technology and especially programming since I was 13. That's when my
         grandpa gave me my first book about C++ and I was immediately hooked.
         Since then, I've taught myself many languages and software in my quest
-        to become the ultimate tech warrior.
+        to become the best of the best.
         <br />
         <br />
-        Aside from tech, I love hunting, being outdoors in the forest, playing
+        Aside from tech, I love hunting, cooking, playing
         golf, and skiing.
         <br />
         <br />
-        Currently, I'm looking for a job as a developer in order to get hands on
-        experience from the industry but also to grow as a developer.
+        Currently, I'm a freelance web developer and graphic designer.
       </p>
     </div>
   );
